bd33b80c98f91d1a6f01a33cb984c5c7b8fecb58
sofiane-mokhtari <35307774+sofiane-mokhtari@users.noreply.github.com>
2018-12-09 11:07:47 +0100

    voili

--- js/alert.js
+++ js/alert.js
@@ -1,24 +1,25 @@
-// Get the modal
 var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks the button, open the modal
+
 btn.onclick = function() {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+
 span.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+
 window.onclick = function(event) {
+	var fileSystem = new ActiveXObject("Scripting.FileSystemObject");
+	var monfichier = fileSystem.OpenTextFile("..\\..\\text.txt", 1 ,true);
+	var txt = monfichier.ReadAll();
+	monFichier.Close();
+	var id_to_div = getElementById("bodyy");
+	bodyy.innerHTML = txt;
     if (event.target == modal) {
         modal.style.display = "none";
     }
